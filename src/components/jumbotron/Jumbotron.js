bc102de11a10051c836bcc1859632c0d2010253d
Dadamuga Mazino <destructicus_lives@yahoo.com>
2020-06-23 13:43:01 +0100

    removed unused import

--- src/components/jumbotron/Jumbotron.js
+++ src/components/jumbotron/Jumbotron.js
@@ -1,14 +1,12 @@
-import React from 'react';
-import PropTypes from 'prop-types';
-import {title, container} from './Jumbotron.module.scss';
+import React from "react";
+import { title, container } from "./Jumbotron.module.scss";
 
-function Jumbotron ({children}) {
-
-    return (
-        <div className={container}>
-            <h1 className={title}>{children}</h1>
-        </div>
-    );
+function Jumbotron({ children }) {
+  return (
+    <div className={container}>
+      <h1 className={title}>{children}</h1>
+    </div>
+  );
 }
 
 export default Jumbotron;
